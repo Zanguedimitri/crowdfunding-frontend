86d22dabe21bf5e8d3bf836ef966af97f2ea9bc8
Dimitri Zangue <dimitrizangue1@gmail.com>
2024-11-09 02:37:08 +0100

    style: add styling for project creation form in modal dialog

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Project } from '../models/project.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
-  private apiUrl = 'http://localhost:3000/projects'; // URL du backend
+  private apiUrl = 'http://localhost:3000/api/v1/projects'; 
 
   constructor(private http: HttpClient) {}
 
-  addProject(projectData: any): Observable<any> {
+  addProject(projectData: Project): Observable<any> {
     return this.http.post(this.apiUrl, projectData);
   }
 }
